6732d9a1690e69ec0953d494c58514d3b274808f
ansekolesnikov <st1mflash@gmail.com>
2024-04-23 21:16:16 +0300

    refactor load-service

--- src/main/java/com/ansekolesnikov/cargologistic/service/main/load/LoadCarService.java
+++ src/main/java/com/ansekolesnikov/cargologistic/service/main/load/LoadCarService.java
@@ -2,28 +2,16 @@ package com.ansekolesnikov.cargologistic.service.main.load;
 
 import com.ansekolesnikov.cargologistic.model.pack.Pack;
 import com.ansekolesnikov.cargologistic.model.car.Car;
-import com.ansekolesnikov.cargologistic.model.car.CarStringInfo;
-import com.ansekolesnikov.cargologistic.model.car.CarUtils;
 import com.ansekolesnikov.cargologistic.model.file.LocalFile;
-import com.ansekolesnikov.cargologistic.model.file.LocalFileImportUtils;
 import com.ansekolesnikov.cargologistic.service.main.CargoService;
 import com.ansekolesnikov.cargologistic.validation.service.LoadCarServiceValidation;
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Service
 public class LoadCarService implements CargoService {
-    private static final Logger LOGGER = Logger.getLogger(LoadCarService.class.getName());
     private static final String PATH_IMPORT_PACKAGES = "src/main/resources/import/packages/";
-    private LocalFile localFile;
-    private String algorithm;
-    private int countCars;
 
     public LoadCarService() {
 
@@ -31,69 +19,24 @@ public class LoadCarService implements CargoService {
 
     @Override
     public String runService(String inputFileName, String inputAlgorithm, String inputCountCars) {
-        initParams(inputFileName, inputAlgorithm, inputCountCars);
+        LocalFile localFile = new LocalFile(PATH_IMPORT_PACKAGES + inputFileName);
+        String algorithm = inputAlgorithm.toLowerCase();
+        int countCars = Integer.parseInt(inputCountCars);
+
         LoadCarServiceValidation serviceValidation = new LoadCarServiceValidation(localFile, algorithm, countCars);
+        LoadCarServiceUtils serviceUtils = new LoadCarServiceUtils();
 
         if (serviceValidation.isValid()) {
-            List<Car> loadedCarList = loadCarsFromFile();
+            List<Pack> importedPackList = serviceUtils.getListPacksFromFile(localFile);
+            List<Car> loadedCarList = serviceUtils.loadCars(importedPackList, countCars, algorithm);
+
             if (serviceValidation.isValidCountCars(loadedCarList)) {
-                return getCarsInfo(loadedCarList);
+                return serviceUtils.getCarsInfo(loadedCarList);
             } else {
                 return serviceValidation.getUserErrorMessage();
             }
         } else {
             return serviceValidation.getUserErrorMessage();
         }
-    }
-
-    private void initParams(String fileName, String algorithm, String countCars) {
-        this.localFile = new LocalFile(PATH_IMPORT_PACKAGES + fileName);
-        this.algorithm = algorithm.toLowerCase();
-        this.countCars = Integer.parseInt(countCars);
-    }
-
-    private List<Car> loadCarsFromFile() {
-        List<Pack> packList = Objects.requireNonNull(new LocalFileImportUtils().importPacksFromFile(localFile))
-                .stream()
-                .sorted(Comparator.comparingInt(Pack::getWidth).reversed())
-                .toList();
-
-        int localCarCount = countCars;
-        List<Car> listCars = new ArrayList<>();
-        do {
-            Car car = new Car();
-            listCars.add(car);
-            packList = packList.stream()
-                    .filter(pack -> pack.getCarId() == 0)
-                    .collect(Collectors.toList());
-
-            for (Pack pack : packList) {
-                car.loadPack(pack, algorithm);
-            }
-            if (localCarCount > 0) {
-                if (new CarUtils().calcPercentLoad(car) == 0) {
-                    LOGGER.info("Грузовик #" + car.getId() + " остался пустым");
-                } else {
-                    LOGGER.info("Грузовик #" + car.getId() + " успешно загружен на " + new CarUtils().calcPercentLoad(car) + "%");
-                }
-            }
-            localCarCount--;
-
-        } while (
-                packList.stream()
-                        .anyMatch(pack -> pack.getCarId() == 0)
-                        || localCarCount > 0
-        );
-        return listCars;
-    }
-
-    private String getCarsInfo(List<Car> listCars) {
-        StringBuilder result = new StringBuilder();
-        if (listCars != null) {
-            for (Car car : listCars) {
-                result.append(new CarStringInfo().getCargo(car)).append("\n");
-            }
-        }
-        return result.toString();
     }
 }
---
+++ src/main/java/com/ansekolesnikov/cargologistic/service/main/load/LoadCarServiceUtils.java
@@ -0,0 +1,66 @@
+package com.ansekolesnikov.cargologistic.service.main.load;
+
+import com.ansekolesnikov.cargologistic.model.car.Car;
+import com.ansekolesnikov.cargologistic.model.car.CarStringInfo;
+import com.ansekolesnikov.cargologistic.model.car.CarUtils;
+import com.ansekolesnikov.cargologistic.model.file.LocalFile;
+import com.ansekolesnikov.cargologistic.model.file.LocalFileImportUtils;
+import com.ansekolesnikov.cargologistic.model.pack.Pack;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class LoadCarServiceUtils {
+    private static final Logger LOGGER = Logger.getLogger(LoadCarServiceUtils.class.getName());
+
+    public String getCarsInfo(List<Car> listCars) {
+        StringBuilder result = new StringBuilder();
+        if (listCars != null) {
+            for (Car car : listCars) {
+                result.append(new CarStringInfo().getCargo(car)).append("\n");
+            }
+        }
+        return result.toString();
+    }
+
+    public List<Pack> getListPacksFromFile(LocalFile localFile) {
+        return Objects.requireNonNull(new LocalFileImportUtils().importPacksFromFile(localFile))
+                .stream()
+                .sorted(Comparator.comparingInt(Pack::getWidth).reversed())
+                .toList();
+    }
+
+    public List<Car> loadCars(List<Pack> packList, int countCars, String algorithm) {
+        int localCarCount = countCars;
+        List<Car> listCars = new ArrayList<>();
+        do {
+            Car car = new Car();
+            listCars.add(car);
+            packList = packList.stream()
+                    .filter(pack -> pack.getCarId() == 0)
+                    .collect(Collectors.toList());
+
+            for (Pack pack : packList) {
+                car.loadPack(pack, algorithm);
+            }
+            if (localCarCount > 0) {
+                if (new CarUtils().calcPercentLoad(car) == 0) {
+                    LOGGER.info("Грузовик #" + car.getId() + " остался пустым");
+                } else {
+                    LOGGER.info("Грузовик #" + car.getId() + " успешно загружен на " + new CarUtils().calcPercentLoad(car) + "%");
+                }
+            }
+            localCarCount--;
+
+        } while (
+                packList.stream()
+                        .anyMatch(pack -> pack.getCarId() == 0)
+                        || localCarCount > 0
+        );
+        return listCars;
+    }
+}
