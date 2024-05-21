67b4e7baa6d92751395b8a2cb403c64a7df06c6d
ansekolesnikov <st1mflash@gmail.com>
2024-05-21 10:17:04 +0300

    Рефакторинг сервисов car и pack, добавление класса интерфейса

--- src/main/java/com/ansekolesnikov/cargologistic/service/cargo/pack/PackService.java
+++ src/main/java/com/ansekolesnikov/cargologistic/service/cargo/pack/PackService.java
@@ -1,12 +1,10 @@
 package com.ansekolesnikov.cargologistic.service.cargo.pack;
 
-import com.ansekolesnikov.cargologistic.database.dao.PackModelDao;
 import com.ansekolesnikov.cargologistic.model.command.CommandLine;
-import com.ansekolesnikov.cargologistic.model.command.pack.PackCommandLine;
 import com.ansekolesnikov.cargologistic.model.pack.PackModel;
 import com.ansekolesnikov.cargologistic.model.pack.utils.PackModelToStringUtils;
-import com.ansekolesnikov.cargologistic.service.cargo.RunnableService;
 import com.ansekolesnikov.cargologistic.service.cargo.EntityService;
+import com.ansekolesnikov.cargologistic.service.cargo.RunnableService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,96 +13,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class PackService implements RunnableService, EntityService {
     @Autowired
-    private PackModelDao packModelDao;
-    @Autowired
     private PackServiceUtils packServiceUtils = new PackServiceUtils();
     @Autowired
     private PackModelToStringUtils packModelToStringUtils = new PackModelToStringUtils();
 
     @Override
     public String runService(CommandLine command) {
-        PackCommandLine packCommandLine = command.getPackCommandLine();
-        return switch (packCommandLine.getOperation()) {
-            case LIST -> toStringAllPackModelsFromDatabase();
-            case INSERT -> insertPackIntoDatabase(packServiceUtils.createPackFromCommand(packCommandLine));
-            case UPDATE -> updatePackInDatabase(findPackByIdInDatabase(packCommandLine.getIdPack()), packCommandLine);
-            case DELETE -> deletePackFromDatabase(findPackByIdInDatabase(packCommandLine.getIdPack()));
+        return switch (command.getPackCommandLine().getOperation()) {
+            case LIST -> listOperation();
+            case INSERT -> insertOperation(command);
+            case UPDATE -> updateOperation(command);
+            case DELETE -> deleteOperation(command);
         };
     }
 
     @Override
-    public String listOperation(){
-        return "";
+    public String listOperation() {
+        return packServiceUtils.queryAllPackModelsToString();
     }
 
     @Override
-    public String insertOperation(CommandLine command){
-        return "";
-    }
-
-    @Override
-    public String updateOperation(CommandLine command){
-        return "";
-    }
-
-    @Override
-    public String deleteOperation(CommandLine command){
-        return "";
-    }
-
-    private PackModel findPackByIdInDatabase(int id) {
-        return packModelDao.findById(id);
-    }
-
-    private String toStringAllPackModelsFromDatabase() {
-        StringBuilder toStringCars = new StringBuilder();
-        for (PackModel packModel : packModelDao.findAll()) {
-            toStringCars
-                    .append(packModelToStringUtils.toStringPackInfo(packModel))
-                    .append("\n\n");
-        }
-        if (toStringCars.isEmpty()) {
-            return "Список моделей посылок пуст." +
-                    "\nДля добавления воспользуйтесь командой: 'pack insert [название] [код] [ширина] [высота]'";
-        } else {
-            return toStringCars.toString();
-        }
-    }
-
-    private String insertPackIntoDatabase(PackModel packModel) {
-        packModelDao.insert(packModel);
+    public String insertOperation(CommandLine command) {
+        PackModel packModel = packServiceUtils.insertPackModelByCommand(command);
         return "Посылка '" + packModel.getName() + "' успешно создана.\n\n"
                 + packModelToStringUtils.toStringPackInfo(packModel);
     }
 
-    private String updatePackInDatabase(PackModel packModel, PackCommandLine command) {
-        PackModel updatedPackModel = packModel;
-        switch (command.getUpdatedParamName()) {
-            case NAME:
-                updatedPackModel.setName(command.getUpdatedParamValue());
-                break;
-            case CODE:
-                updatedPackModel.setCode(command.getUpdatedParamValue().charAt(0));
-                break;
-            case SCHEME:
-                updatedPackModel.setScheme(command.getUpdatedParamValue());
-                break;
-            case WIDTH:
-                updatedPackModel.setWidth(Integer.parseInt(command.getUpdatedParamValue()));
-                break;
-            case HEIGHT:
-                updatedPackModel.setHeight(Integer.parseInt(command.getUpdatedParamValue()));
-                break;
-            default:
-                break;
-        }
-        packModelDao.update(updatedPackModel);
+    @Override
+    public String updateOperation(CommandLine command) {
+        PackModel packModel = packServiceUtils.updatePackModelByCommand(command);
         return "Посылка '" + packModel.getName() + "' успешно обновлена.\n\n"
                 + packModelToStringUtils.toStringPackInfo(packModel);
     }
 
-    private String deletePackFromDatabase(PackModel packModel) {
-        packModelDao.delete(packModel);
+    @Override
+    public String deleteOperation(CommandLine command) {
+        PackModel packModel = packServiceUtils.deletePackModelByCommand(command);
         return "Посылка '" + packModel.getName() + "' успешно удалена.";
     }
 }
--- src/main/java/com/ansekolesnikov/cargologistic/service/cargo/pack/PackServiceUtils.java
+++ src/main/java/com/ansekolesnikov/cargologistic/service/cargo/pack/PackServiceUtils.java
@@ -1,14 +1,21 @@
 package com.ansekolesnikov.cargologistic.service.cargo.pack;
 
+import com.ansekolesnikov.cargologistic.database.dao.PackModelDao;
+import com.ansekolesnikov.cargologistic.model.command.CommandLine;
 import com.ansekolesnikov.cargologistic.model.command.pack.PackCommandLine;
 import com.ansekolesnikov.cargologistic.model.pack.PackModel;
+import com.ansekolesnikov.cargologistic.model.pack.utils.PackModelToStringUtils;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
 @Component
 public class PackServiceUtils {
-    public PackModel createPackFromCommand(PackCommandLine command){
+    @Autowired
+    private PackModelDao packModelDao;
+
+    public PackModel createPackModelFromCommand(PackCommandLine command) {
         return new PackModel(
                 command.getNamePack(),
                 command.getWidthSchemePack(),
@@ -16,5 +23,60 @@ public class PackServiceUtils {
                 command.getSchemePack(),
                 command.getCodePack()
         );
+    }
+
+    public String queryAllPackModelsToString() {
+        StringBuilder toStringCars = new StringBuilder();
+        for (PackModel packModel : packModelDao.findAll()) {
+            toStringCars
+                    .append(new PackModelToStringUtils().toStringPackInfo(packModel))
+                    .append("\n\n");
+        }
+        if (toStringCars.isEmpty()) {
+            return "Список моделей посылок пуст." +
+                    "\nДля добавления воспользуйтесь командой: 'pack insert [название] [код] [ширина] [высота]'";
+        } else {
+            return toStringCars.toString();
+        }
+    }
+
+    public PackModel insertPackModelByCommand(CommandLine inputCommand) {
+        PackCommandLine command = inputCommand.getPackCommandLine();
+        PackModel packModel = createPackModelFromCommand(command);
+        packModelDao.insert(packModel);
+        return packModel;
+    }
+
+    public PackModel updatePackModelByCommand(CommandLine inputCommand) {
+        PackCommandLine command = inputCommand.getPackCommandLine();
+        PackModel packModel = packModelDao.findById(command.getIdPack());
+        switch (command.getUpdatedParamName()) {
+            case NAME:
+                packModel.setName(command.getUpdatedParamValue());
+                break;
+            case CODE:
+                packModel.setCode(command.getUpdatedParamValue().charAt(0));
+                break;
+            case SCHEME:
+                packModel.setScheme(command.getUpdatedParamValue());
+                break;
+            case WIDTH:
+                packModel.setWidth(Integer.parseInt(command.getUpdatedParamValue()));
+                break;
+            case HEIGHT:
+                packModel.setHeight(Integer.parseInt(command.getUpdatedParamValue()));
+                break;
+            default:
+                break;
+        }
+        packModelDao.update(packModel);
+        return packModel;
+    }
+
+    public PackModel deletePackModelByCommand(CommandLine inputCommand) {
+        PackCommandLine command = inputCommand.getPackCommandLine();
+        PackModel packModel = packModelDao.findById(command.getIdPack());
+        packModelDao.delete(packModel);
+        return packModel;
     }
 }
