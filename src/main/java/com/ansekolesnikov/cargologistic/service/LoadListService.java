e88847ac8a85bbf364ac6efc9b8945335f04d75f
ansekolesnikov <st1mflash@gmail.com>
2024-05-27 11:43:15 +0300

    Рефакторинг LoadListService - убрал LoadListServiceUtils.java

--- src/main/java/com/ansekolesnikov/cargologistic/service/LoadListService.java
+++ src/main/java/com/ansekolesnikov/cargologistic/service/LoadListService.java
@@ -2,19 +2,25 @@ package com.ansekolesnikov.cargologistic.service;
 
 import com.ansekolesnikov.cargologistic.database.dao.CarModelDao;
 import com.ansekolesnikov.cargologistic.database.dao.PackModelDao;
+import com.ansekolesnikov.cargologistic.entity.Car;
 import com.ansekolesnikov.cargologistic.entity.CarModel;
-import com.ansekolesnikov.cargologistic.service.service_input.ServiceInput;
-import com.ansekolesnikov.cargologistic.service.service_input.LoadListServiceInput;
 import com.ansekolesnikov.cargologistic.entity.Pack;
-import com.ansekolesnikov.cargologistic.service.service_output.ServiceOutput;
+import com.ansekolesnikov.cargologistic.entity.PackModel;
+import com.ansekolesnikov.cargologistic.entity.utils.CarToStringUtils;
+import com.ansekolesnikov.cargologistic.entity.utils.CarUtils;
+import com.ansekolesnikov.cargologistic.enums.AlgorithmEnum;
 import com.ansekolesnikov.cargologistic.interfaces.RunnableService;
-import com.ansekolesnikov.cargologistic.service.utils.LoadListServiceUtils;
+import com.ansekolesnikov.cargologistic.service.service_input.LoadListServiceInput;
+import com.ansekolesnikov.cargologistic.service.service_input.ServiceInput;
 import com.ansekolesnikov.cargologistic.service.service_output.LoadListServiceOutput;
+import com.ansekolesnikov.cargologistic.service.service_output.ServiceOutput;
 import lombok.NoArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @NoArgsConstructor
@@ -25,7 +31,9 @@ public class LoadListService implements RunnableService {
     @Autowired
     private PackModelDao packModelDao;
     @Autowired
-    private LoadListServiceUtils loadListServiceUtils;
+    private CarUtils carUtils;
+    @Autowired
+    private CarToStringUtils carToStringUtils;
 
     private static final Logger LOGGER = Logger.getLogger(LoadListService.class.getName());
 
@@ -36,27 +44,75 @@ public class LoadListService implements RunnableService {
             LoadListServiceInput command = serviceInput.getLoadListServiceInput();
             CarModel carModel = carModelDao.findByName(command.getCarModel());
             List<Pack> pack =
-                    loadListServiceUtils
-                            .createPacksByNameFromDatabase(
-                                    packModelDao,
-                                    command.getPacks()
-                            );
+                    createPacksByNameFromDatabase(
+                            packModelDao,
+                            command.getPacks()
+                    );
 
-            result.setText(
-                    loadListServiceUtils.toStringCarsPacksInfo(
+            result.setText(toStringCarsPacksInfo(
+                    pack,
+                    loadCars(
+                            carModel,
                             pack,
-                            loadListServiceUtils.loadCars(
-                                    carModel,
-                                    pack,
-                                    command.getCountCars(),
-                                    command.getAlgorithm()
-                            )
-                    ));
+                            command.getCountCars(),
+                            command.getAlgorithm()
+                    )
+            ));
             return result;
         } catch (RuntimeException e) {
             LOGGER.error("Ошибка ввода команды.");
             result.setText("Ошибка ввода.");
             return result;
         }
+    }
+
+    public List<Car> loadCars(
+            CarModel inputCarModel,
+            List<Pack> inputPack,
+            int inputCountCars,
+            AlgorithmEnum inputAlgorithm
+    ) {
+        List<Car> listCars = new ArrayList<>();
+
+        for (int i = 0; i < inputCountCars; i++) {
+            Car car = new Car(inputCarModel);
+            listCars.add(car);
+
+            List<Pack> filteredPackList = inputPack.stream()
+                    .filter(pack -> pack.getCarId() == 0)
+                    .toList();
+
+            for (Pack pack : filteredPackList) {
+                carUtils.loadPackToCar(car, pack, inputAlgorithm);
+            }
+        }
+        return listCars;
+    }
+
+    public String toStringCarsPacksInfo(List<Pack> pack, List<Car> listCars) {
+        StringBuilder result = new StringBuilder();
+
+        for (PackModel packModel : pack) {
+            result.append(packModel.getCode()).append(" -- ").append(packModel.getName()).append("\n");
+        }
+        result.append("\n");
+
+        if (listCars != null) {
+            for (Car car : listCars) {
+                result.append(carToStringUtils.toStringCarCargoScheme(car)).append("\n");
+            }
+        }
+        return result.toString();
+    }
+
+    public List<Pack> createPacksByNameFromDatabase(
+            PackModelDao packModelDao,
+            String[] packNames
+    ) {
+        return Arrays
+                .stream(packNames)
+                .map(packModelDao::findByName)
+                .map(Pack::new)
+                .toList();
     }
 }
